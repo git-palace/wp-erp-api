4480e657c5b67cff4539c2014a4a6465feb0669e
git-palace <edeveloper4@gmail.com>
2019-03-26 04:18:16 -0700

    post api endpoint for multiple contacts

--- includes/class-contact.php
+++ includes/class-contact.php
@@ -24,6 +24,11 @@ class Contact_API_Handler {
 			'methods'	=> 'POST',
 			'callback'	=> [ $this, 'post_contact' ]
 		] );
+
+		register_rest_route( 'wp-erp-api', 'add-contacts', [
+			'methods'	=> 'POST',
+			'callback'	=> [ $this, 'add_contacts' ]
+		] );
 	}
 
 	// /wp-erp-api/contact/:id for 1 contact
@@ -58,5 +63,23 @@ class Contact_API_Handler {
 		}
 
 		wp_send_json_success( 'Updated successfully.' );
+	}
+
+	// add contacts by posted data.
+	// array of these keys : photo_id,first_name,last_name,email,phone,life_stage,contact_owner,date_of_birth,contact_age,mobile,website,fax,street_1,street_2,city,country,state,postal_code,source,other,notes,facebook,twitter,googleplus,linkedin,user_id,type,
+	function add_contacts( $request ) {
+		$user = check_authentication();
+
+		$contacts = json_decode( $request->get_body(), true );
+
+		foreach ( $contacts as $contact ) {
+			$people_id = erp_insert_people( $contact );
+
+			if ( is_wp_error( $people_id ) ) {
+				wp_send_json_error( 'Non contact with the id = ' . $request['id'] );
+			}
+		}
+
+		wp_send_json_success( 'Added successfully.' );
 	}
 }
