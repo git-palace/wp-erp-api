aef643f9e314f6abdd65d83046bbca5909b0ea9b
git-palace <edeveloper4@gmail.com>
2019-04-03 01:19:19 -0700

    add broker

---
+++ includes/class-user.php
@@ -0,0 +1,95 @@
+<?php
+
+class User_API_Handler {
+	private static $instance = null;
+
+	static function get_instance() {
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new User_API_Handler();
+		}
+
+		return self::$instance;
+	}
+
+	function __construct() {
+		add_action( 'rest_api_init', [$this, 'api_route_register'] );
+	}
+
+	function api_route_register() {
+		register_rest_route( 'wp-erp-api', 'broker(?:/(?P<id>\d+))?', [
+			'methods' 	=> 'GET',
+			'callback' 	=> [ $this, 'get_broker' ]
+		] );
+
+		register_rest_route( 'wp-erp-api', 'agent(?:/(?P<id>\d+))?', [
+			'methods' 	=> 'GET',
+			'callback' 	=> [ $this, 'get_agent' ]
+		] );
+
+		register_rest_route( 'wp-erp-api', 'staff(?:/(?P<id>\d+))?', [
+			'methods' 	=> 'GET',
+			'callback' 	=> [ $this, 'get_staff' ]
+		] );
+
+		register_rest_route( 'wp-erp-api', 'team(?:/(?P<id>\d+))?', [
+			'methods' 	=> 'GET',
+			'callback' 	=> [ $this, 'get_team' ]
+		] );
+	}
+
+	// get broker
+	function get_broker( $request ) {
+		$user = check_authentication();
+
+		if ( !current_user_can( 'administrator' ) ) {
+			wp_send_json_success( ( new WeDevs\ERP\HRM\Employee( get_current_user_id() ) )->to_Array() );
+		}
+
+	    $args = $_REQUEST;
+
+		if ( isset( $request['id'] ) || !empty( $request['id'] ) ) {
+			$employee    = new WeDevs\ERP\HRM\Employee( $request['id'] );
+
+			if ( ! $employee->is_employee() )
+	            wp_send_json_error( 'Broker does not exists.' );
+	        
+	        $is_staff_or_team_user = get_user_meta( $request['id'], 'is_staff_or_team_user', true );
+	        $owner_id = get_user_meta( $request['id'], 'created_by', true );
+
+	        if ( $is_staff_or_team_user == "off" && user_can( $owner_id, 'administrator' ) )
+				wp_send_json_success( $employee->to_Array() );
+			else
+				wp_send_json_error( 'Broker does not exists.' );
+		}
+
+		$employees = erp_hr_get_employees( $args );
+		$employee_list = array();
+
+		foreach ( $employees as $idx => $employee ) {
+			$is_staff_or_team_user = get_user_meta( $employee->user_id, 'is_staff_or_team_user', true );
+	        $owner_id = get_user_meta( $employee->user_id, 'created_by', true );
+
+			if ( $is_staff_or_team_user == "off" && user_can( $owner_id, 'administrator' ) ) {
+				array_push( $employee_list, ( new WeDevs\ERP\HRM\Employee( $employee->user_id ) )->to_Array() );
+			}
+		}
+
+		wp_send_json_success( $employee_list );
+	}
+
+	// get staff
+	function get_staff( $request ) {
+		$user = check_authentication();
+	}
+
+	// get agent
+	function get_agent( $request ) {
+		$user = check_authentication();		
+	}
+
+	// get team
+	function get_team( $request ) {
+		$user = check_authentication();
+	}
+}
+?>
--- includes/index.php
+++ includes/index.php
@@ -3,6 +3,8 @@ class WP_ERP_API_Handler {
 	private static $instance = null;
 	private $contact_api_handler = null;
 	private $company_api_handler = null;
+	private $circle_api_handler = null;
+	private $user_api_handler = null;
 
 	public static function get_instance() {
 		if ( is_null( self::$instance ) )
@@ -15,9 +17,11 @@ class WP_ERP_API_Handler {
 		require_once 'class-contact.php';
 		require_once 'class-company.php';
 		require_once 'class-circle.php';
+		require_once 'class-user.php';
 
 		$this->contact_api_handler = Contact_API_Handler::get_instance();
 		$this->company_api_handler = Company_API_Handler::get_instance();
 		$this->circle_api_handler = Circle_API_Handler::get_instance();
+		$this->user_api_handler = User_API_Handler::get_instance();
 	}
 }
