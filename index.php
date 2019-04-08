d2c9bb2a007677931b16c874b9313a6dea24999f
git-palace <edeveloper4@gmail.com>
2019-04-08 10:11:54 -0700

    add apache request header func

--- index.php
+++ index.php
@@ -10,6 +10,30 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 if ( !is_plugin_active( 'wp-erp/wp-erp.php' ) )
     return;
 
+if( !function_exists('apache_request_headers') ) {
+    function apache_request_headers() {
+        $arh = array();
+        $rx_http = '/\AHTTP_/';
+        foreach($_SERVER as $key => $val) {
+            if( preg_match($rx_http, $key) ) {
+                $arh_key = preg_replace($rx_http, '', $key);
+                $rx_matches = array();
+                // do some nasty string manipulations to restore the original letter case
+                // this should work in most cases
+                $rx_matches = explode('_', strtolower($arh_key));
+                if( count($rx_matches) > 0 and strlen($arh_key) > 2 ) {
+                        foreach($rx_matches as $ak_key => $ak_val) $rx_matches[$ak_key] = ucfirst($ak_val);
+                        $arh_key = implode('-', $rx_matches);
+                }
+                $arh[$arh_key] = $val;
+            }
+        }
+        if(isset($_SERVER['CONTENT_TYPE'])) $arh['Content-Type'] = $_SERVER['CONTENT_TYPE'];
+        if(isset($_SERVER['CONTENT_LENGTH'])) $arh['Content-Length'] = $_SERVER['CONTENT_LENGTH'];
+        return( $arh );
+    }
+}
+
 function check_authentication() {
 	$headers = apache_request_headers();
     
